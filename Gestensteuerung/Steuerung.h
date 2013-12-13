63eac119ccb62a09a235b9869642661089cd0756
StefanieStoppel <s.stoppel@gmx.de>
2013-12-13 18:30:26 +0100

    Aktualisierung Steuerung.h

--- Gestensteuerung/Steuerung.h
+++ Gestensteuerung/Steuerung.h
@@ -5,12 +5,19 @@ class Steuerung{
 public:
 	Steuerung();
 	~Steuerung();
-	void initialize(float sampleRate);
-	float processOneSample(float);
-	void process(float* input, float*output, int numberOfSamples);
-	void setGain(float gain);
+	void initialize(); //"Controller-Erkennung" geschieht hier (?)
+	float getXPositionChange(); //gibt Änderung der x-Position zurück. Positiver Wert => Verschiebung nach rechts, Negativer Wert => Verschiebung nach links
+	float getXPosition(); //gibt aktuelle X-Position zurück. Diese Berechnet sich aus dem Mittelwert der Positionen der weißen Pixel in der Binärmaske.
+	void convertToBinary(cv::Mat videoFrame, cv::Mat& processedFrame); //Konvertiert jedes Frame in ein Binärframe, wobei der "Controller" weiß ist, der Hintergrund schwarz.
+	cv::Point centroidOfWhitePixels(const cv::Mat& image); //Berechnet den "Mittelpunkt" der weißen Pixel in der Binärmaske und gibt seine Position zurück (als Point-Objekt)
+	void process(); //Ein Frame wird verarbeitet. "Main-Loop" dieser Klasse.
 private:
-	float sampleRate;
-	float gain;
+	float xPosition; //aktuelle horizontale Position des Controllers
+	float xPositionChange;//horizontale Positionsänderung seit dem letzten Frame. Kann positiv oder negativ sein.
+	cv::Mat controllerFrame;//von der Webcam aufgenommene Frames
+	cv::Mat controllerBinFrame;//von der Webcam aufgenommene Frames als Binärbilder
+	cv::Mat lastFrame; //das zuvor aufgenommene Frame in Graustufen. Dient zum Erstellen des Binärbilds.
+	int frameNumber;
+
 };
 #endif
