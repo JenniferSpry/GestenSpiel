27858bc1195170c22edbf31f2a10e284e6423120
JenniferSpry <jennifer.spry@web.de>
2013-12-15 19:09:51 +0100

    Added temporary background image and turned it into a moving background.

--- Gestensteuerung/main.cpp
+++ Gestensteuerung/main.cpp
@@ -3,13 +3,39 @@
 using namespace cv;
 using namespace std;
 
-public void loop(){
-	//do stuff
-	//comment
+Mat bg;
+
+void init(){
+	bg = imread("img/bg.png", 1);
+	namedWindow( "Bienchen & Blümchen", 1 ); 	
+}
+
+void move(){
+	Mat temp; 
+	bg.copyTo(temp);
+	Vec3b blue(0, 0, 255);
+	bg.at<Vec3b>(50, 50) = blue;
+	for (int x = 0; x < (bg.rows-1); x++){
+		temp.row(x).copyTo(bg.row(x+1));
+	}
+	temp.row(bg.rows-1).copyTo(bg.row(0));
+}
+
+void loop(){
+	while(true){
+		move();
+		imshow( "Bienchen & Blümchen", bg );
+		// end
+		int key = waitKey(30);
+		if (key != -1){
+			break;
+		}
+	}
 }
 
 
 int main(){
+	init();
 	loop();
 	return 0;
 }
