2cfb8eeb0c51cc288c1a7c76741c1a57e300a5bb
JenniferSpry <jennifer.spry@web.de>
2013-12-17 16:31:55 +0100

    speed of movement is now system independant

--- Gestensteuerung/main.cpp
+++ Gestensteuerung/main.cpp
@@ -1,5 +1,7 @@
 #include <opencv2\opencv.hpp>
 #include <string>
+#include <ctime>
+#include "windows.h"
 #include "Steuerung.h"
 #include "Bee.h"
 using namespace cv;
@@ -9,21 +11,32 @@ Mat bg;
 Mat viewImage;
 Steuerung steuerung;
 Bee bee;
+clock_t lastFrame;
+
+double getDelta() {
+	long currentTime = clock();
+	double delta = (double) currentTime - (double) lastFrame;
+	lastFrame = clock();
+	return delta;
+}
 
 void init(){
+	lastFrame = clock();
 	steuerung.initialize();
 	bee.init();
 	bg = imread("img/bg.png", 1);
 	namedWindow( "Bienchen & Blümchen", 1 ); 	
 }
 
-void moveBG(){
+void moveBG(int delta){
 	Mat temp; 
 	bg.copyTo(temp);
-	for (int x = 0; x < (bg.rows-1); x++){
-		temp.row(x).copyTo(bg.row(x+1));
+	for (int x = 0; x < (bg.rows-delta); x++){
+		temp.row(x).copyTo(bg.row(x+delta));
 	}
-	temp.row(bg.rows-1).copyTo(bg.row(0));
+	for (int j = 0; j < delta; j++){
+		temp.row(bg.rows-delta+j).copyTo(bg.row(j));
+	}
 }
 
 void loop(){
@@ -31,7 +44,9 @@ void loop(){
 		if (!steuerung.process()){
 			break;
 		}
-		moveBG();
+		//collisionmanagement
+		int delta = getDelta()*0.2;
+		moveBG(delta);
 		bg.copyTo(viewImage);
 		imshow( "Bienchen & Blümchen", bee.insertInto(viewImage));
 		// end
@@ -41,7 +56,6 @@ void loop(){
 		}
 	}
 }
-
 
 
 int main(){
